0ac7a81ed97aaf7aeb6f4ba1752a46f51572839c
Caio S. Souza <caiobiounb@gmail.com>
2018-06-28 12:43:17 -0300

    Single fitness implementation

--- package/miga/src/Population.cpp
+++ package/miga/src/Population.cpp
@@ -1,9 +1,9 @@
 #include "Population.hpp"
+#include "CPUPopulation.hpp"
 
 Population *make_population(const std::string platform) {
     if (platform == "CPU") {
-        return nullptr;
-        //return new CPUPopulation();
+        return new CPUPopulation();
 
     } else if (platform == "GPU") {
         return nullptr;
