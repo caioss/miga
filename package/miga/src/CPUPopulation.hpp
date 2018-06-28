6808a76815e292b5bda13e7c57a947ca5804bc14
Caio S. Souza <caiobiounb@gmail.com>
2018-06-28 16:26:30 -0300

    Added platform interface

--- package/miga/src/CPUPopulation.hpp
+++ package/miga/src/CPUPopulation.hpp
@@ -5,7 +5,20 @@
 
 class CPUPopulation : public Population {
 public:
+    std::string platform() const { return "CPU"; };
     data_t singleFitness(const seq_t index) const;
+
+private:
+    seq_t _q;
+    seq_t _numSeqs;
+    seq_t _numICA;
+    seq_t _numICB;
+    data_t _lambda;
+    seq_t *_genome;
+    seq_t *_seqA;
+    seq_t *_seqB;
+    data_t *_siteProbsA;
+    data_t *_siteProbsB;
 };
 
 #endif
---
+++ package/miga/types.pxd
@@ -0,0 +1,8 @@
+import numpy as np
+from libcpp cimport bool
+from libc.stdint cimport uint32_t
+
+ctypedef uint32_t seq_t;
+ctypedef double data_t;
+
+np_seq_t = np.uint32
