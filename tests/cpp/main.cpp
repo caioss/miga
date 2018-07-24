d4a9cc5ed46ab9708984c715e9ad231404720bed
Caio S. Souza <caiobiounb@gmail.com>
2018-07-23 23:59:28 -0300

    Added Population typed tests

--- tests/cpp/main.cpp
+++ tests/cpp/main.cpp
@@ -1,4 +1,7 @@
-int main(int argv, char *argc[])
+#include "gtest/gtest.h"
+
+int main(int argc, char *argv[])
 {
-    return 0;
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
