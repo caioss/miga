e8fc8b3fedf59fe9e3a5c6b5a15dcd5bc61f7b27
Caio S. Souza <caiobiounb@gmail.com>
2018-09-21 19:48:26 -0300

    Added script to update version and updated version to 1.0.0

--- docs/python/source/conf.py
+++ docs/python/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Caio S. Souza'
 author = 'Caio S. Souza'
 
 # The short X.Y version
-version = ''
+version = "1.0"
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------
